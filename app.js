2ad30c2ee863e0c342eaea200db98f25ccb6f0d6
saisanthoshreddysingareddy <109932454+saisanthoshreddysingareddy@users.noreply.github.com>
2022-08-13 07:17:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -63,18 +63,18 @@ app.get("/movies/:movieId/", async (request, response) => {
   response.send(changingNames(getSingleResponse));
 });
 
-app.post("/movies/", (request, response) => {
+app.post("/movies/",async (request, response) => {
   const { directorId, movieName, leadActor } = request.body;
   const postQuery = `
     INSERT INTO 
         movie(director_id,movie_name,lead_actor)
     VALUES(
         ${directorId},'${movieName}','${leadActor}');`;
-  const postResponse = db.run(postQuery);
+  const postResponse =await db.run(postQuery);
   response.send("Movie Successfully Added");
 });
 
-app.put("/movies/:movieId/", (request, response) => {
+app.put("/movies/:movieId/",async (request, response) => {
   const { movieId } = request.params;
   const { directorId, movieName, leadActor } = request.body;
   const putQuery = `
@@ -82,15 +82,15 @@ app.put("/movies/:movieId/", (request, response) => {
     SET director_id=${directorId},
     movie_name='${movieName}',
     lead_actor='${leadActor}';`;
-  const putResponse = db.run(putQuery);
+  const putResponse =await db.run(putQuery);
   response.send("Movie Details Updated");
 });
-app.delete("/movies/:movieId/", (request, response) => {
+app.delete("/movies/:movieId/",async (request, response) => {
   const { movieId } = request.params;
   const deleteQuery = `
     DELETE FROM movie
     WHERE movie_id=${movieId};`;
-  const deleteResponse = db.run(deleteQuery);
+  const deleteResponse =await db.run(deleteQuery);
   response.send("Movie Removed");
 });
 
